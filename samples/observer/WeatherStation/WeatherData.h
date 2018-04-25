2b54bfa22ad8b0a648ab4ee27d92911c9ba590c9
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-04-25 15:03:41 +0300

    Add wind sensor

--- samples/observer/WeatherStation/WeatherData.h
+++ samples/observer/WeatherStation/WeatherData.h
@@ -4,15 +4,30 @@
 #include <algorithm>
 #include <climits>
 #include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include "Observer.h"
 
 using namespace std;
+
+struct SWindData
+{
+	double speed = 0;
+	double direction = 0;
+};
+
+ostream& operator<<(ostream &stream, SWindData const& wind)
+{
+	stream << "speed " << wind.speed << " direction " << wind.direction;
+	return stream;
+}
 
 struct SWeatherInfo
 {
 	double temperature = 0;
 	double humidity = 0;
 	double pressure = 0;
+	SWindData wind;
 };
 
 class CDisplay: public IObserver<SWeatherInfo>
@@ -27,14 +42,17 @@ private:
 		std::cout << "Current Temp " << data.temperature << std::endl;
 		std::cout << "Current Hum " << data.humidity << std::endl;
 		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "Current Wind " << data.wind << std::endl;
 		std::cout << "----------------" << std::endl;
 	}
 };
 
-class CStatsCalculator
+class CMinMaxStatsCalculator
 {
 public:
-	void AddValue(double value)
+	virtual ~CMinMaxStatsCalculator() = default;
+
+	virtual void AddValue(double value)
 	{
 		if (m_minValue > value)
 		{
@@ -44,8 +62,6 @@ public:
 		{
 			m_maxValue = value;
 		}
-		m_accValue += value;
-		++m_countAcc;
 	}
 
 	double GetMinValue() const
@@ -58,15 +74,72 @@ public:
 		return m_maxValue;
 	}
 
+private:
+	double m_minValue = std::numeric_limits<double>::infinity();
+	double m_maxValue = -std::numeric_limits<double>::infinity();
+};
+
+class CScalarStatsCalculator : public CMinMaxStatsCalculator
+{
+public:
+	void AddValue(double value) final
+	{
+		CMinMaxStatsCalculator::AddValue(value);
+		m_accValue += value;
+		++m_countAcc;
+	}
+
 	double GetAverageValue() const
 	{
 		return m_accValue / m_countAcc;
 	}
 
 private:
-	double m_minValue = std::numeric_limits<double>::infinity();
-	double m_maxValue = -std::numeric_limits<double>::infinity();
 	double m_accValue = 0;
+	unsigned m_countAcc = 0;
+};
+
+double sqr(double a)
+{
+	return a * a;
+}
+
+class CWindStatsCalculator
+{
+public:
+	void AddValue(SWindData const& data)
+	{
+		m_minMaxSpeedStat.AddValue(data.speed);
+
+		const double radAngle = data.direction * M_PI / 180;
+		m_accValue.first += data.speed * cos(radAngle);
+		m_accValue.second += data.speed * sin(radAngle);
+
+		++m_countAcc;
+	}
+
+	double GetMinSpeed() const
+	{
+		return m_minMaxSpeedStat.GetMinValue();
+	}
+
+	double GetMaxSpeed() const
+	{
+		return m_minMaxSpeedStat.GetMaxValue();
+	}
+
+	SWindData GetAverageValue() const
+	{
+		SWindData result;
+		double radAngle = atan2(m_accValue.second, m_accValue.first);
+		result.direction = radAngle * 180 / M_PI;
+		result.speed = sqrt(sqr(m_accValue.first) + sqr(m_accValue.second)) / m_countAcc;
+		return result;
+	}
+
+private:
+	CMinMaxStatsCalculator m_minMaxSpeedStat;
+	std::pair<double, double> m_accValue = { 0, 0 };
 	unsigned m_countAcc = 0;
 };
 
@@ -82,23 +155,33 @@ private:
 		m_temperatureStats.AddValue(data.temperature);
 		m_humidityStats.AddValue(data.humidity);
 		m_pressureStats.AddValue(data.pressure);
+		m_windStats.AddValue(data.wind);
 
-		DisplayStats("Temp", m_temperatureStats);
-		DisplayStats("Hum", m_humidityStats);
-		DisplayStats("Pressure", m_pressureStats);
+		DisplayScalarStats("Temp", m_temperatureStats);
+		DisplayScalarStats("Hum", m_humidityStats);
+		DisplayScalarStats("Pressure", m_pressureStats);
+		DisplayWindStats(m_windStats);
 		std::cout << "----------------" << std::endl;
 	}
 
-	void DisplayStats(std::string const& name, CStatsCalculator const& stats)
+	static void DisplayScalarStats(std::string const& name, CScalarStatsCalculator const& stats)
 	{
 		std::cout << "Max " << name << " " << stats.GetMaxValue() << std::endl;
 		std::cout << "Min " << name << " " << stats.GetMinValue() << std::endl;
 		std::cout << "Average " << name << " " << stats.GetAverageValue() << std::endl;
 	}
 
-	CStatsCalculator m_temperatureStats;
-	CStatsCalculator m_humidityStats;
-	CStatsCalculator m_pressureStats;
+	static void DisplayWindStats(CWindStatsCalculator const& stats)
+	{
+		std::cout << "Max Wind speed " << stats.GetMaxSpeed() << std::endl;
+		std::cout << "Min Wind speed " << stats.GetMinSpeed() << std::endl;
+		std::cout << "Average Wind " << stats.GetAverageValue() << std::endl;
+	}
+
+	CScalarStatsCalculator m_temperatureStats;
+	CScalarStatsCalculator m_humidityStats;
+	CScalarStatsCalculator m_pressureStats;
+	CWindStatsCalculator m_windStats;
 };
 
 class CWeatherData : public CObservable<SWeatherInfo>
@@ -119,17 +202,24 @@ public:
 	{
 		return m_pressure;
 	}
+	// Скорость и направление ветра
+	SWindData GetWind()const
+	{
+		return m_wind;
+	}
 
 	void MeasurementsChanged()
 	{
 		NotifyObservers();
 	}
 
-	void SetMeasurements(double temp, double humidity, double pressure)
+	void SetMeasurements(double temp, double humidity, double pressure, double windSpeed, double windDirection)
 	{
 		m_humidity = humidity;
 		m_temperature = temp;
 		m_pressure = pressure;
+		m_wind.speed = windSpeed;
+		m_wind.direction = windDirection;
 
 		MeasurementsChanged();
 	}
@@ -140,10 +230,12 @@ protected:
 		info.temperature = GetTemperature();
 		info.humidity = GetHumidity();
 		info.pressure = GetPressure();
+		info.wind = GetWind();
 		return info;
 	}
 private:
 	double m_temperature = 0.0;
 	double m_humidity = 0.0;	
 	double m_pressure = 760.0;	
+	SWindData m_wind;
 };
--- samples/observer/WeatherStation/main.cpp
+++ samples/observer/WeatherStation/main.cpp
@@ -10,12 +10,12 @@ int main()
 	CStatsDisplay statsDisplay;
 	wd.RegisterObserver(statsDisplay, 0);
 
-	wd.SetMeasurements(3, 0.7, 760);
-	wd.SetMeasurements(4, 0.8, 761);
+	wd.SetMeasurements(3, 0.7, 760, 2, 0);
+	wd.SetMeasurements(4, 0.8, 761, 3, 10);
 
 	wd.RemoveObserver(statsDisplay);
 
-	wd.SetMeasurements(10, 0.8, 761);
-	wd.SetMeasurements(-10, 0.8, 761);
+	wd.SetMeasurements(10, 0.8, 761, 5, 100);
+	wd.SetMeasurements(-10, 0.8, 761, 7, 145);
 	return 0;
 }
