e69284f9b7d4689ed1cbd55e583d07b1c3a419d0
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-05-03 01:53:28 +0300

    Use map to pick shape creation function

--- samples/factory/libpainter/ShapeFactory.cpp
+++ samples/factory/libpainter/ShapeFactory.cpp
@@ -19,35 +19,52 @@ void ThrowOnWrongArgumentCount(std::string const& name, size_t expected, size_t 
 	}
 }
 
+auto CreateRectangle(CUtils::ParseShapeResult const& shape)
+{
+	const auto& ints = shape.ints;
+	ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
+	return std::make_unique<CRectangle>(shape.color, CPoint(ints[0], ints[1]), CPoint(ints[2], ints[3]));
+}
+
+auto CreateTriangle(CUtils::ParseShapeResult const& shape)
+{
+	const auto& ints = shape.ints;
+	ThrowOnWrongArgumentCount(shape.type, 6, ints.size());
+	return std::make_unique<CTriangle>(shape.color, CPoint(ints[0], ints[1]), CPoint(ints[2], ints[3]), CPoint(ints[4], ints[5]));
+}
+
+auto CreateEllipse(CUtils::ParseShapeResult const& shape)
+{
+	const auto& ints = shape.ints;
+	ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
+	return std::make_unique<CEllipse>(shape.color, CPoint(ints[0], ints[1]), ints[2], ints[3]);
+}
+
+auto CreatePolygon(CUtils::ParseShapeResult const& shape)
+{
+	const auto& ints = shape.ints;
+	ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
+	return std::make_unique<CRegularPolygon>(shape.color, ints[0], CPoint(ints[1], ints[2]), ints[3]);
+}
+
+const std::map<std::string, std::function<std::unique_ptr<CShape>(CUtils::ParseShapeResult const&)>> NAME_TO_FUNCTION_MAP = {
+	{ "rectangle", CreateRectangle },
+	{ "triangle", CreateTriangle },
+	{ "ellipse", CreateEllipse },
+	{ "polygon", CreatePolygon },
+};
+
 }
 
 std::unique_ptr<CShape> CShapeFactory::CreateShape(const std::string & description)
 {
 	const auto shape = CUtils::ParseShape(description);
-	const auto& ints = shape.ints;
+	const auto it = NAME_TO_FUNCTION_MAP.find(shape.type);
 
-	if (shape.type == "rectangle")
-	{
-		ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
-		return std::make_unique<CRectangle>(shape.color, CPoint(ints[0], ints[1]), CPoint(ints[2], ints[3]));
-	}
-	else if (shape.type == "triangle")
-	{
-		ThrowOnWrongArgumentCount(shape.type, 6, ints.size());
-		return std::make_unique<CTriangle>(shape.color, CPoint(ints[0], ints[1]), CPoint(ints[2], ints[3]), CPoint(ints[4], ints[5]));
-	}
-	else if (shape.type == "ellipse")
-	{
-		ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
-		return std::make_unique<CEllipse>(shape.color, CPoint(ints[0], ints[1]), ints[2], ints[3]);
-	}
-	else if (shape.type == "polygon")
-	{
-		ThrowOnWrongArgumentCount(shape.type, 4, ints.size());
-		return std::make_unique<CRegularPolygon>(shape.color, ints[0], CPoint(ints[1], ints[2]), ints[3]);
-	}
-	else
+	if (it == NAME_TO_FUNCTION_MAP.end())
 	{
 		throw std::runtime_error("Invalid shape name: " + shape.type);
 	}
+
+	return it->second(shape);
 }
