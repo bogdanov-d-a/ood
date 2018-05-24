c743a45f8c1dd2600f7566f752d3babf47b66096
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-05-24 19:57:40 +0300

    Add stream decoration

--- samples/decorator/streams/main.cpp
+++ samples/decorator/streams/main.cpp
@@ -1,10 +1,80 @@
 ﻿#include "../libstreams/deps.h"
 #include "../libstreams/FileInputStream.h"
 #include "../libstreams/FileOutputStream.h"
+#include "../libstreams/OutputStreamEncryptor.h"
+#include "../libstreams/InputStreamDecryptor.h"
+#include "../libstreams/OutputStreamCompressor.h"
+#include "../libstreams/InputStreamDecompressor.h"
+
+using IntCallback = std::function<void(int)>;
+using VoidCallback = std::function<void()>;
+
+namespace
+{
+
+enum class ArgParseState
+{
+	DEFAULT,
+	ENCRYPT_KEY,
+	DECRYPT_KEY,
+};
+
+void ParseArgs(int argc, char *argv[],
+	IntCallback const& onEncrypt, IntCallback const& onDecrypt,
+	VoidCallback const& onCompress, VoidCallback const& onDecompress)
+{
+	ArgParseState state = ArgParseState::DEFAULT;
+
+	for (int curArgIndex = 1; curArgIndex < argc - 2; ++curArgIndex)
+	{
+		std::string curArg(argv[curArgIndex]);
+
+		switch (state)
+		{
+		case ArgParseState::DEFAULT:
+			if (curArg == "--encrypt")
+			{
+				state = ArgParseState::ENCRYPT_KEY;
+			}
+			else if (curArg == "--decrypt")
+			{
+				state = ArgParseState::DECRYPT_KEY;
+			}
+			else if (curArg == "--compress")
+			{
+				onCompress();
+			}
+			else if (curArg == "--decompress")
+			{
+				onDecompress();
+			}
+			else
+			{
+				throw std::invalid_argument(curArg);
+			}
+			break;
+
+		case ArgParseState::ENCRYPT_KEY:
+			onEncrypt(std::stoi(curArg));
+			state = ArgParseState::DEFAULT;
+			break;
+
+		case ArgParseState::DECRYPT_KEY:
+			onDecrypt(std::stoi(curArg));
+			state = ArgParseState::DEFAULT;
+			break;
+
+		default:
+			throw std::runtime_error("bad parser state");
+		}
+	}
+}
+
+}
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		std::cout << "Not enough arguments" << std::endl;
 		return 0;
@@ -13,12 +83,27 @@ int main(int argc, char *argv[])
 	const std::string inputName(argv[argc - 2]);
 	const std::string outputName(argv[argc - 1]);
 
-	FileInputStream input(inputName);
-	FileOutputStream output(outputName);
+	IInputDataStreamPtr input = std::make_unique<FileInputStream>(inputName);
+	IOutputDataStreamPtr output = std::make_unique<FileOutputStream>(outputName);
 
-	while (!input.IsEOF())
+	ParseArgs(argc, argv,
+		[&output](int key) {
+			output = std::make_unique<OutputStreamEncryptor>(std::move(output), key);
+		},
+		[&input](int key) {
+			input = std::make_unique<InputStreamDecryptor>(std::move(input), key);
+		},
+		[&output]() {
+			output = std::make_unique<OutputStreamCompressor>(std::move(output));
+		},
+		[&input]() {
+			input = std::make_unique<InputStreamDecompressor>(std::move(input));
+		}
+	);
+
+	while (!input->IsEOF())
 	{
-		output.WriteByte(input.ReadByte());
+		output->WriteByte(input->ReadByte());
 	}
 
 	return 0;
