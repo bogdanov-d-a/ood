e29d13c851c98d0229706d4f8ce9b2ccab4adc03
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-14 00:16:52 +0300

    Resolve potential UB issue

--- samples/command/iSpringWord/ISpringWord/InsertParagraphCommand.cpp
+++ samples/command/iSpringWord/ISpringWord/InsertParagraphCommand.cpp
@@ -3,17 +3,36 @@
 
 InsertParagraphCommand::InsertParagraphCommand(DocumentData &data, std::string const & text, const boost::optional<size_t>& position)
 	: m_data(data)
-	, m_text(text)
+	, m_executeData(text)
 	, m_position(position)
 {
 }
 
 void InsertParagraphCommand::DoExecute()
 {
-	m_data.InsertParagraph(m_text, m_position);
+	if (auto text = boost::get<std::string>(&m_executeData))
+	{
+		m_data.InsertParagraph(*text, m_position);
+	}
+	else if (auto paragraph = boost::get<std::shared_ptr<CParagraph>>(&m_executeData))
+	{
+		m_data.InsertItem(std::move(*paragraph), m_position);
+	}
+	else
+	{
+		assert(false);
+	}
+	m_executeData = Empty();
 }
 
 void InsertParagraphCommand::DoUnexecute()
 {
-	m_data.DeleteItem(m_position);
+	assert(m_executeData.type() == typeid(Empty));
+
+	auto item = m_data.DeleteItem(m_position);
+
+	auto paragraphPtr = boost::get<std::shared_ptr<CParagraph>>(&item);
+	assert(paragraphPtr);
+
+	m_executeData = std::move(*paragraphPtr);
 }
--- samples/command/iSpringWord/ISpringWord/InsertParagraphCommand.h
+++ samples/command/iSpringWord/ISpringWord/InsertParagraphCommand.h
@@ -9,10 +9,15 @@ public:
 	explicit InsertParagraphCommand(DocumentData &data, std::string const& text, const boost::optional<size_t>& position);
 
 private:
+	struct Empty
+	{};
+
+	using ExecuteData = boost::variant<Empty, std::string, std::shared_ptr<CParagraph>>;
+
 	void DoExecute() final;
 	void DoUnexecute() final;
 
 	DocumentData &m_data;
-	const std::string m_text;
+	ExecuteData m_executeData;
 	const boost::optional<size_t> m_position;
 };
