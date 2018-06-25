9e90c42d395f35fc52d420a1673b1e888693e93e
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-25 21:11:50 +0300

    Improve ICanvas interface

--- samples/composite/composite/Canvas.h
+++ samples/composite/composite/Canvas.h
@@ -4,11 +4,16 @@
 class ICanvas
 {
 public:
+	virtual void SetFillColor(RGBAColor color) = 0;
+	virtual void ResetFillColor() = 0;
+
 	virtual void SetLineColor(RGBAColor color) = 0;
-	virtual void BeginFill(RGBAColor color) = 0;
-	virtual void EndFill() = 0;
-	virtual void MoveTo(double x, double y) = 0;
-	virtual void LineTo(double x, double y) = 0;
+	virtual void ResetLineColor() = 0;
+
+	virtual void SetLineThickness(double thickness) = 0;
+	virtual void ResetLineThickness() = 0;
+
+	virtual void DrawPolygon(std::vector<PointD> const& points) = 0;
 	virtual void DrawEllipse(double left, double top, double width, double height) = 0;
 
 	virtual ~ICanvas() = 0;
--- samples/composite/composite/CommonTypes.h
+++ samples/composite/composite/CommonTypes.h
@@ -1,6 +1,13 @@
 #pragma once
 
 template <typename T>
+struct Point
+{
+	T x;
+	T y;
+};
+
+template <typename T>
 struct Rect
 {
 	T left;
@@ -9,5 +16,7 @@ struct Rect
 	T height;
 };
 
+typedef Point<double> PointD;
 typedef Rect<double> RectD;
+
 typedef uint32_t RGBAColor;
