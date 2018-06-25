b9e02bc0a0830222c646195f61081f5b02ccfc99
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-25 22:02:28 +0300

    Add ILineStyle

---
+++ samples/composite/composite/ILineStyle.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CommonTypes.h"
+
+class ILineStyle
+{
+public:
+	virtual ~ILineStyle() = default;
+
+	virtual bool IsEnabled() const = 0;
+	virtual void Enable(bool enable) = 0;
+
+	virtual RGBAColor GetColor() const = 0;
+	virtual void SetColor(RGBAColor color) = 0;
+
+	virtual double GetThickness() const = 0;
+	virtual void SetThickness() = 0;
+};
