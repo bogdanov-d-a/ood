703b398906816be0c08c58806d05c72e26f61025
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-07-17 22:32:42 +0300

    Add more tests

--- samples/state/gumball_machine_tests/Tests.cpp
+++ samples/state/gumball_machine_tests/Tests.cpp
@@ -99,3 +99,79 @@ BOOST_AUTO_TEST_CASE(CanBuyGumball)
 		"A gumball comes rolling out the slot...",
 	}));
 }
+
+BOOST_AUTO_TEST_CASE(CanBuyLastGumball)
+{
+	std::vector<std::string> log;
+	with_state::CGumballMachine m(1, [&log](std::string const& message) {
+		log.push_back(message);
+	});
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	BOOST_CHECK(log == std::vector<std::string>({
+		"You inserted a quarter",
+		"You turned...",
+		"A gumball comes rolling out the slot...",
+		"Oops, out of gumballs",
+		"You can't insert a quarter, the machine is sold out",
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(CanBuyGumballTwice)
+{
+	std::vector<std::string> log;
+	with_state::CGumballMachine m(2, [&log](std::string const& message) {
+		log.push_back(message);
+	});
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	BOOST_CHECK(log == std::vector<std::string>({
+		"You inserted a quarter",
+		"You turned...",
+		"A gumball comes rolling out the slot...",
+		"You inserted a quarter",
+		"You turned...",
+		"A gumball comes rolling out the slot...",
+		"Oops, out of gumballs",
+		"You can't insert a quarter, the machine is sold out",
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(CantEjectQuarterAfterBuy)
+{
+	std::vector<std::string> log;
+	with_state::CGumballMachine m(42, [&log](std::string const& message) {
+		log.push_back(message);
+	});
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.EjectQuarter();
+	BOOST_CHECK(log == std::vector<std::string>({
+		"You inserted a quarter",
+		"You turned...",
+		"A gumball comes rolling out the slot...",
+		"You haven't inserted a quarter",
+	}));
+}
+
+BOOST_AUTO_TEST_CASE(CantDispenseExtraGumball)
+{
+	std::vector<std::string> log;
+	with_state::CGumballMachine m(42, [&log](std::string const& message) {
+		log.push_back(message);
+	});
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.TurnCrank();
+	BOOST_CHECK(log == std::vector<std::string>({
+		"You inserted a quarter",
+		"You turned...",
+		"A gumball comes rolling out the slot...",
+		"You turned but there's no quarter",
+		"You need to pay first",
+	}));
+}
